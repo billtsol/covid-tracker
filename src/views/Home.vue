200359c8a00db67883f2efb647b86b676200f7e6
billtsol <billtsol@protonmail.com>
2021-10-02 22:45:16 +0300

    first commit

---
+++ src/views/Home.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="home">Hello world</div>
+</template>
+
+<script>
+export default {
+  name: "Home",
+  components: {}
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
+//13.00
