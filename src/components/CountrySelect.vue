51e5f1d6e0a08c63271ba3a25f6f3e3e397ccec7
billtsol <billtsol@protonmail.com>
2021-10-03 12:07:51 +0300

    New commit

---
+++ src/components/CountrySelect.vue
@@ -0,0 +1,36 @@
+<template>
+  <select
+    @change="onChange"
+    v-model="selected"
+    class="form-select mt-5 block w-full border p-3 rounded"
+  >
+    <option value="0">Select Country</option>
+    <option v-for="country in countries" :key="country.ID" :value="country.ID">
+      {{ country.Country }}
+    </option>
+  </select>
+</template>
+
+<script>
+export default {
+  name: "CountrySelect",
+  props: {
+    countries: {
+      type: Array
+    }
+  },
+  data() {
+    return {
+      selected: 0
+    };
+  },
+  methods: {
+    onChange() {
+      const countryData = this.countries.find(
+        item => item.ID === this.selected
+      );
+      this.$emit("get-country", countryData);
+    }
+  }
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,79 @@
 <template>
-  <div class="home">Hello world</div>
+  <main v-if="!loading">
+    <DataTitle :text="title" :dataDate="dataDate" />
+    <DataBoxes :stats="stats" />
+
+    <CountrySelect @get-country="getCountryData" :countries="countries" />
+
+    <button
+      @click="clearCountryData"
+      v-if="stats.Country !== Global"
+      class="
+        bg-green-700
+        text-white
+        rounded
+        p-3
+        mt-5
+        focus:outline-none
+        hover:bg-green-600
+      "
+    >
+      Clear Countries
+    </button>
+  </main>
+
+  <main class="flex flex-col align-center justify-center text-center" v-else>
+    <div class="text-gray-500 text-3xl mt-10 mb-6">fetching Data</div>
+
+    <div class="loading w-24 m-auto"></div>
+  </main>
 </template>
 
 <script>
+import DataTitle from "@/components/DataTitle";
+import DataBoxes from "@/components/DataBoxes";
+import CountrySelect from "@/components/CountrySelect";
 export default {
   name: "Home",
-  components: {}
+  components: {
+    DataTitle,
+    DataBoxes,
+    CountrySelect
+  },
+  data() {
+    return {
+      loading: true,
+      title: "Global",
+      dataDate: "",
+      stats: {},
+      countries: []
+    };
+  },
+  methods: {
+    async fetchCovidData() {
+      const res = await fetch("https://api.covid19api.com/summary");
+      const data = await res.json();
+      return data;
+    },
+    getCountryData(country) {
+      this.stats = country;
+      this.title = country.Country;
+    },
+    async clearCountryData() {
+      this.loading = true;
+      const data = await this.fetchCovidData();
+      this.title = "Global";
+      this.stats = data.Global;
+      this.loading = false;
+    }
+  },
+  async created() {
+    const data = await this.fetchCovidData();
+    this.dataDate = data.Date;
+    this.stats = data.Global;
+    this.countries = data.Countries;
+    this.loading = false;
+  }
 };
 </script>
 
